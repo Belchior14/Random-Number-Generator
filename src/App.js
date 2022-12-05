3dc74233d1f0238bba454c6030a4d63e9200bb01
Belchior <belchior.fontao@gmail.com>
2022-12-05 16:57:37 +0000

    backend logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [minNumber , setMinNumber] = useState(0)
+
+  const [maxNumber , setMaxNumber] = useState(1)
+
+  const [randomNumber, setRandomNumber] = useState()
+
+  const handleRandom = (e) => {
+    e.preventDefault()
+    console.log("min number",typeof +minNumber)
+    console.log("max number" , maxNumber)
+    console.log("some", +minNumber + +maxNumber)
+    setRandomNumber(Math.floor(Math.random() * (+maxNumber - +minNumber +1) + +minNumber))
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div>
+        <label>Please choose the lowest number:</label>
+        <input type="number" value={minNumber} onChange = {(e) => setMinNumber(e.target.value)}/>
+      </div>
+
+      
+      <div>
+        <label>Please choose the highest number:</label>
+        <input type="Number" value={maxNumber} onChange = {(e) => setMaxNumber(e.target.value)}/>
+      </div>
+
+      <div>
+        <form onSubmit={handleRandom}>
+        <h4>Get you random number here!</h4>
+        <button>Click!</button>
+        </form>
+        <h3>{randomNumber}</h3>
+      </div>
     </div>
   );
 }
